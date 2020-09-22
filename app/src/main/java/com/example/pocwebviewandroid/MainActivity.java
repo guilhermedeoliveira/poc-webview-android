793f6883b997778796870f4369ba6c43b0ffbcb4
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2020-09-22 11:10:42 -0300

    feat: organize code

--- app/src/main/java/com/example/pocwebviewandroid/MainActivity.java
+++ app/src/main/java/com/example/pocwebviewandroid/MainActivity.java
@@ -1,46 +1,7 @@
 package com.example.pocwebviewandroid;
 
-// package mgks.os.fileup;
-
-/*
- * Os-FileUp is an Open Source Android Project hosted on GitHub (https://github.com/mgks/Os-FileUp).
- * Developed by Ghazi Khan (https://github.com/mgks) under MIT Open Source License.
- * This program is free to use for private and commercial purposes.
- * Please donate to the project if it helped you (https://ko-fi.com/getmgks).
- * Don't forget to mention project source or developer credit in your Application's License(s) Wiki.
- * Giving right credit to developers encourages them to create better projects :)
- */
-
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.ClipData;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-
-import android.webkit.ValueCallback;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -55,10 +16,6 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
         webView.addJavascriptInterface(new WebAppInterface(this), "Android");
 
-        // WebViewClientImpl webViewClient = new WebViewClientImpl(this);
-        // webView.setWebViewClient(webViewClient);
-
         webView.loadUrl("https://poc-webview-web.guilhermedeoliveira.vercel.app/");
-
     }
 }
--- app/src/main/java/com/example/pocwebviewandroid/WebAppInterface.java
+++ app/src/main/java/com/example/pocwebviewandroid/WebAppInterface.java
@@ -2,31 +2,17 @@ package com.example.pocwebviewandroid;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.provider.MediaStore;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
-
-import java.io.File;
-import java.io.IOException;
 
 import static androidx.core.app.ActivityCompat.startActivityForResult;
 
 public class WebAppInterface {
     Context mContext;
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
     /** Instantiate the interface and set the context */
     WebAppInterface(Context c) {
         mContext = c;
-    }
-
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
     }
 
     /** Show a toast from the web page */
@@ -35,4 +21,3 @@ public class WebAppInterface {
         Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
     }
 }
-
